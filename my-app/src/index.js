15fd86e243e966cbd27da5e84e2232ec624a672a
evnwttn <evnwttn@gmail.com>
2022-02-01 12:55:31 -0330

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,8 +2,80 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+let states = [
+  ["Alabama", "Montgomery"],
+  ["Alaska", "Juneau"],
+  ["Arizona", "Phoenix"],
+  ["Arkansas", "Little Rock"],
+  ["California", "Sacramento"],
+  ["Colorado", "Denver"],
+  ["Connecticut", "Hartford"],
+  ["Delaware", "Dover"],
+  ["Florida", "Tallahassee"],
+  ["Georgia", "Atlanta"],
+  ["Hawaii", "Honolulu"],
+  ["Idaho", "Boise"],
+  ["Illinois", "Springfield"],
+  ["Indiana", "Indianapolis"],
+  ["Iowa", "Des Moines"],
+  ["Kansas", "Topeka"],
+  ["Kentucky", "Frankfort"],
+  ["Louisiana", "Baton Rouge"],
+  ["Maine", "Augusta"],
+  ["Maryland", "Annapolis"],
+  ["Massachusetts", "Boston"],
+  ["Michigan", "Lansing"],
+  ["Minnesota", "Saint Paul"],
+  ["Mississippi", "Jackson"],
+  ["Missouri", "Jefferson City"],
+  ["Montana", "Helena"],
+  ["Nebraska", "Lincoln"],
+  ["Nevada", "Carson City"],
+  ["New Hampshire", "Concord"],
+  ["New Jersey", "Trenton"],
+  ["New Mexico", "Santa Fe"],
+  ["New York", "Albany"],
+  ["North Carolina", "Raleigh"],
+  ["North Dakota", "Bismarck"],
+  ["Ohio", "Columbus"],
+  ["Oklahoma", "Oklahoma"],
+  ["Oregon", "Salem"],
+  ["Pennsylvania", "Harrisburg"],
+  ["Rhode Island", "Providence"],
+  ["South Carolina", "Columbia"],
+  ["South Dakota", "Pierre"],
+  ["Tennessee", "Nashville"],
+  ["Texas", "Austin"],
+  ["Utah", "Salt Lake City"],
+  ["Vermont", "Montpelier"],
+  ["Virginia", "Richmond"],
+  ["Washington", "Olympia"],
+  ["West Virginia", "Charleston"],
+  ["Wisconson", "Madison"],
+  ["Wyoming", "Cheyenne"],
+];
+
+function ThrowStates() {
+  let arr = [];
+  let r;
+  while (arr.length < 3) {
+    r = Math.floor(Math.random() * states.length) + 1;
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
+  let stateArr = [states[arr[0]], states[arr[1]], states[arr[2]]];
+
+  console.log(stateArr);
+
+  return null;
+}
+
 export default function GideonSundback() {
-  return <>Sometimes it do be like that.</>;
+  return (
+    <>
+      Sometimes it do be like that.
+      <ThrowStates />
+    </>
+  );
 }
 
 ReactDOM.render(GideonSundback(), document.getElementById("root"));
